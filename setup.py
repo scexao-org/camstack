1a4923659cafe47138d1993fa7c8613b6470cfa1
vdeo@scexao1 <vdeo@naoj.org>
2021-04-27 02:58:25 +0000

    make_cset_and_rt and Makefile

--- setup.py
+++ setup.py
@@ -8,6 +8,17 @@ deps:
 '''
 
 from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from subprocess import check_call
+
+import os
+
+class PreInstallCommand(develop):
+    def run(self):
+        os.system('cd camstack/core; sudo make clean all; cd ../..')
+        # A manual copy to the PATH maybe necessary - it doesn't seem the script line works for this.
+        develop.run(self)
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -22,4 +33,7 @@ setup(
         url = "http://www.github.com/milk-org/camstack",
         packages = ['camstack'],  # same as name
         install_requires = ['docopt', 'libtmux', 'pygame'],
-        scripts = ['./buffycam.py', './chuckcam.py'])
+        scripts = ['./viewers/buffycam.py', './viewers/chuckcam.py'],#, './camstack/core/make_cset_and_rt'],
+        cmdclass={'develop': PreInstallCommand},
+    )
+
