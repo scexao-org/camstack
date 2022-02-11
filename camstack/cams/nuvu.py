b5ea2f8e010d9be05ad4d5083d065900e2489af2
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-02-11 14:32:15 +0100

    SHM keywaord updates before shm creation disabled

--- camstack/cams/nuvu.py
+++ camstack/cams/nuvu.py
@@ -188,7 +188,7 @@ class NUVU(EDTCamera):
             return False
         (success,resdict) = self.send_command("ld %d"%(self.RO_MODES.index(romode)), timeout=400.)
         if success:
-            self.camera_shm.update_keyword('DETMODE', romode)
+            #self.camera_shm.update_keyword('DETMODE', romode)
             self.cfgdict.update(resdict)
         else:
             logging.error("Error setting Readoutmode")
