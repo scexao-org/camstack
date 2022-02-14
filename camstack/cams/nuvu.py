ffb2008657bf20fbcc7eaac3189e44a505ff6936
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-02-14 15:44:56 +0100

    Setting up for 70x512 image size

--- camstack/cams/nuvu.py
+++ camstack/cams/nuvu.py
@@ -34,7 +34,7 @@ class NUVU(EDTCamera):
         # FULL 128 x 128
         FULL: CameraMode(x0=0, x1=127, y0=0, y1=127),
         0: CameraMode(x0=0, x1=127, y0=0, y1=127, fps=1500., tint=0.0006),
-        1: CameraMode(x0=0, x1=520, y0=0, y1=72, fps=1500., tint=0.0006, fgsize=(520,72)),
+        1: CameraMode(x0=0, x1=520, y0=0, y1=70, fps=1500., tint=0.0006, fgsize=(520,70)),
     }
 
     KEYWORDS = {}
