f64dee4deb4d953bb72036e5e1de5238a7fd2f09
vdeo <vdeo@naoj.org>
2022-09-01 09:52:57 -1000

    Binning in anycam.py

--- viewers/anycam.py
+++ viewers/anycam.py
@@ -3,10 +3,11 @@
     pygame SHM viewer - generic basic version
 
     Usage:
-        anycam.py <shm_name> [-z <zoom>]
+        anycam.py <shm_name> [-z <zoom>] [-b <binn>]
 
     Options:
-        -z <zoom>    System zoom factor [default: 1]
+        -z <zoom>    Graphics windows factor [default: 1]
+        -b <binn>    SHM binning factor [default: 1]
 '''
 
 import docopt
@@ -17,11 +18,16 @@ from camstack.viewers.generic_viewer_backend import GenericViewerBackend
 if __name__ == '__main__':
 
     args = docopt.docopt(__doc__)
+    
     zoom = int(args['-z'])
+    binn = int(args['-b'])
     shm_name = args['<shm_name>']
 
     backend = GenericViewerBackend(shm_name)
 
-    frontend = GenericViewerFrontend(zoom, 20, backend.shm_shape)
+    binned_backend_shape = (backend.shm_shape[0] // binn,
+    backend.shm_shape[1] // binn)
+
+    frontend = GenericViewerFrontend(zoom, 20, binned_backend_shape)
     frontend.register_backend(backend)
     frontend.run()
