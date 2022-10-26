d77690d5789ba4cfad2627126dcdbf1b81e39012
vdeo <vdeo@naoj.org>
2022-10-25 18:02:36 -1000

    Fix logger common

--- camstack/core/logger.py
+++ camstack/core/logger.py
@@ -1,12 +1,13 @@
 import os
 import logging as logg
+from logging import handlers as logghandlers
 
 
 def init_camstack_logger(file_fullpath: str, file_debug: bool = False) -> None:
     stdouthandler = logg.StreamHandler(os.sys.stdout)
     stdouthandler.setLevel(logg.WARNING)
     
-    logfilehandler = logg.RotatingFileHandler(file_fullpath, maxBytes=1048576, backupCount=5) # TODO rotating files, max length ???
+    logfilehandler = logghandlers.RotatingFileHandler(file_fullpath, maxBytes=1048576, backupCount=5) # TODO rotating files, max length ???
     logfilehandler.setLevel((logg.INFO, logg.DEBUG)[file_debug])
 
     handlers = [stdouthandler, logfilehandler]
