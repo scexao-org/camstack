2b7a0d2b7f0d12aacb2d194af9abf05c47ae5a56
vdeo@sonne <vdeo@naoj.org>
2023-01-03 08:38:43 +0000

    Flycap cams: fix allowed control of tint and fps

--- camstack/cam_mains/vpupcam.py
+++ camstack/cam_mains/vpupcam.py
@@ -1,11 +1,11 @@
 from camstack.cams.flycapturecam import VampiresPupilFlea
 
+from camstack.core.utilities import shellify_methods
+
 DEFAULT_SHM_NAME = "vpupcam"
 
-def main():
-    # start running backend
-    cam = VampiresPupilFlea("vpup", DEFAULT_SHM_NAME, "FULL", 0)
-
-
 if __name__ == "__main__":
-    main()
+    cam = VampiresPupilFlea("vpup", DEFAULT_SHM_NAME, "CROP_VPUP", 0)
+
+    from camstack.core.utilities import shellify_methods
+    shellify_methods(cam, globals())
