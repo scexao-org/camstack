a373d337b90f9b80298f4f1d86b09012bbd13861
Miles Lucas <mdlucas@hawaii.edu>
2023-06-07 05:02:44 +0000

    tidy up vpupcam

--- camstack/cam_mains/vpupcam.py
+++ camstack/cam_mains/vpupcam.py
@@ -1,11 +1,16 @@
 from camstack.cams.flycapturecam import VampiresPupilFlea
 
 from camstack.core.utilities import shellify_methods
+from scxconf import PYRONS3_HOST, PYRONS3_PORT, IP_VAMPIRES
+from camstack import pyro_keys as pk
+from swmain.network.pyroserver_registerable import PyroServer
 
 DEFAULT_SHM_NAME = "vpupcam"
 
 if __name__ == "__main__":
     cam = VampiresPupilFlea("vpup", DEFAULT_SHM_NAME, "CROP_VPUP", 0)
-
-    from camstack.core.utilities import shellify_methods
     shellify_methods(cam, globals())
+    server = PyroServer(bindTo=(IP_VAMPIRES, 0),
+                        nsAddress=(PYRONS3_HOST, PYRONS3_PORT))
+    server.add_device(cam, pk.VPUPCAM, add_oneway_callables=True)
+    server.start()
--- camstack/viewers/vampires/vpupcam.py
+++ camstack/viewers/vampires/vpupcam.py
@@ -15,6 +15,9 @@ from rich.live import Live
 from rich.logging import RichHandler
 
 logger = logging.getLogger("vpupcam")
+stream_handler = RichHandler(level=logging.INFO, show_level=False,
+                             show_path=False, log_time_format="%H:%M:%S")
+logger.addHandler(stream_handler)
 
 
 class VAMPIRESPupilCamViewerFrontend(GenericViewerFrontend):
@@ -23,37 +26,29 @@ class VAMPIRESPupilCamViewerFrontend(GenericViewerFrontend):
 
 class VAMPIRESPupilCamViewerBackend(GenericViewerBackend):
     HELP_MSG = """VPUPCAM controls
-    ---------------
-    h           : display this message
-    x, ESC      : quit vpupcam
+---------------
+h           : display this message
+x, ESC      : quit vpupcam
 
-logger = logging.getLogger("vpupcam")
-logger.setLevel(logging.INFO)
-stream_handler = RichHandler(
-    level=logging.INFO, show_level=False, show_path=False, log_time_format="%H:%M:%S"
-)
-logger.addHandler(stream_handler)
-
-
-    pupil wheel controls:
-    ---------------------
-    CTRL+ -- :  change filter wheel slot
-            1:  Open (0 deg)
-            2:  SAM-7
-            3:  SAM-9
-            4:  SAM-18
-            5:  SAM-18-Nudged
-            6:  SAM-Ann-Nudged
-            7:  Mirror
-            8:  SAM-Ann
-            9:  LyotStop
-            0:  Open (218 deg)
-            -:  ND10
-            =:  ND25
-    CTRL+ARROW :  Nudge wheel 0.01 mm in x (left/right) and y (up/down)
-    SHIFT+ARROW:  Move wheel 1 mm in x (left/right) and y (up/down)
-    CTRL+[]    :  Nudge wheel 0.1 deg in theta (ccw/cw)
-    SHIFT+[]   :  Nudge wheel 1 deg in theta (ccw/cw)
+pupil wheel controls:
+---------------------
+CTRL+ -- :  change filter wheel slot
+        1:  Open (0 deg)
+        2:  SAM-7
+        3:  SAM-9
+        4:  SAM-18
+        5:  SAM-18-Nudged
+        6:  SAM-Ann-Nudged
+        7:  Mirror
+        8:  SAM-Ann
+        9:  LyotStop
+        0:  Open (218 deg)
+        -:  ND10
+        =:  ND25
+CTRL+ARROW :  Nudge wheel 0.01 mm in x (left/right) and y (up/down)
+SHIFT+ARROW:  Move wheel 1 mm in x (left/right) and y (up/down)
+CTRL+[]    :  Nudge wheel 0.1 deg in theta (ccw/cw)
+SHIFT+[]   :  Nudge wheel 1 deg in theta (ccw/cw)
     """
 
     # CTRL+S:  Save current position to preset
@@ -63,8 +58,8 @@ logger.addHandler(stream_handler)
     def __init__(self, name_shm=None):
         if name_shm is None:
             name_shm = "vpupcam"
+        super().__init__(name_shm=name_shm)
         self.wheel = connect("VAMPIRES_MASK")
-        return super().__init__(name_shm=name_shm)
 
 
 class MaskStatusPlugin(BasePlugin):
@@ -72,15 +67,16 @@ class MaskStatusPlugin(BasePlugin):
     def __init__(self, frontend_obj: GenericViewerFrontend) -> None:
         super().__init__(frontend_obj)
         zoom = self.frontend_obj.system_zoom
-        font = pygame.font.SysFont("default", 50 * zoom)
+        font = pygame.font.SysFont("default", 40 * zoom)
         self.enabled = True
         # Ideally you'd instantiate the label in the frontend, cuz different viewers could be wanting the same info
         # displayed at different locations.
         self.label = futs.LabelMessage(
                 "%s", font, fg_col="#4AC985", bg_col=None,
-                topright=(self.frontend_obj.data_disp_size[0] - 200 * zoom,
-                          self.frontend_obj.data_disp_size[1] - 50 * zoom))
+                topleft=(20 * zoom,
+                         self.frontend_obj.data_disp_size[1] - 40 * zoom))
         self.label.blit(self.frontend_obj.pg_datasurface)
+        self.current_index = None
 
         # yapf: disable
         self.shortcut_map = {
@@ -108,6 +104,7 @@ class MaskStatusPlugin(BasePlugin):
             buts.Shortcut(pgmc.K_0, pgmc.KMOD_LCTRL): partial(self.change_wheel, 10),
             buts.Shortcut(pgmc.K_MINUS, pgmc.KMOD_LCTRL): partial(self.change_wheel, 11),
             buts.Shortcut(pgmc.K_EQUALS, pgmc.KMOD_LCTRL): partial(self.change_wheel, 12),
+            buts.Shortcut(pgmc.K_s, pgmc.KMOD_LCTRL): self.save_config,
             }
         # yapf: enable
 
@@ -127,11 +124,11 @@ class MaskStatusPlugin(BasePlugin):
             sign = -1
 
         if fine:
-            nudge_value = sign * 0.01
+            nudge_value = sign * 0.02
         else:
             nudge_value = sign * 1
         logger.info(f"Moving {substage} by {nudge_value} mm")
-        self.wheel.move_relative__oneway(substage, nudge_value)
+        self.backend_obj.wheel.move_relative__oneway(substage, nudge_value)
 
     def rotate_wheel(self, key, fine=True):
         # CCW
@@ -146,16 +143,23 @@ class MaskStatusPlugin(BasePlugin):
         else:
             nudge_value = sign * 1
         logger.info(f"Rotating theta by {nudge_value} deg")
-        self.wheel.move_relative__oneway("theta", nudge_value)
+        self.backend_obj.wheel.move_relative__oneway("theta", nudge_value)
 
     def change_wheel(self, index: int):
         logger.info(f"Moving wheel to configuration {index}")
-        self.wheel.move_configuration_idx__oneway(index)
+        self.backend_obj.wheel.move_configuration_idx__oneway(index)
+        self.current_index = index
 
-    def save_config(self, index: int):
+    def save_config(self):
+        if self.current_index is None:
+            logger.info(
+                    "Must have selected a mask before saving its configuration")
+            return
+        else:
+            index = self.current_index
         logger.info(f"Saving position for configuration {index}")
-        self.wheel.save_configuration(index=index)
-        self.wheel.update_keys()
+        self.backend_obj.wheel.save_configuration(index=index)
+        self.backend_obj.wheel.update_keys()
 
     def frontend_action(self) -> None:
         self.label.render(self.status,
