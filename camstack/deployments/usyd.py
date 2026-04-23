5e8afb86b6fda47297df2f0e344db97bc593b994
vdeo <vdeo@naoj.org>
2026-04-23 22:35:48 +0200

    deployment: fix cam key to VISPG1

--- camstack/deployments/usyd.py
+++ camstack/deployments/usyd.py
@@ -4,6 +4,6 @@ from ..main_arch import CameraLauncherSpec
 
 USYD_VISPL_CONFIG = [
         CameraLauncherSpec(\
-            name='FIRST', main='camstack.cam_mains.usyd',
+            name='VISPG1', main='camstack.cam_mains.usyd',
             add_args=['VPG1'], conda_env_string='mamba activate py38'),
 ]
